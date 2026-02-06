e753d9ed181a7f73775d1ef5559e3ca5afa4f972
SanjaySrinivas4599 <sanjaysrinivas333@gmail.com>
2026-02-06 18:49:11 +0530

    Create function.cpp

---
+++ function.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int max_of_four(int a, int b, int c, int d) {
+    return max({a,b,c,d});
+}
+
+int main() {    
+    int a,b,c,d;
+    
+    cin >> a >> b >> c >> d;
+    cout << max_of_four(a, b, c, d) << endl;
+    return 0;
+}
